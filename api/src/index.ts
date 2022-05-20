0777ac94eb9bab2f16399654d89cd5f986bef04f
Subin_S22 <ssubin615@gmail.com>
2022-05-20 18:30:23 +0530

    mongodb setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-app.listen(4000, () => main());
+app.listen(process.env.PORT, () => main());
 
+const url = process.env.mongodb_url as string;
 function main() {
-	console.log("main");
+  mongoose.connect(url, () => {
+    console.log("connected", url);
+  });
 }

3e27527c008123b9049a25f3bb23c573fdae15a1
Subin_S22 <ssubin615@gmail.com>
2022-05-27 17:52:33 +0530

    web login

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,15 +3,17 @@ import "./App.css";
 import HomePage from "./pages/homepage";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from "./pages/login";
+import { Navigate } from "react-router-dom";
 
 const App: React.FC = () => {
+  const isLogin = localStorage.getItem('login-status');
   return (
     <div id="app">
       <BrowserRouter>
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={isLogin ? <Navigate to={'/home'} replace /> : <Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -3,11 +3,26 @@ import { Formik, Form, Field, ErrorMessage } from 'formik'
 import Button from 'src/modules/button/button'
 import styles from '../styles/login.module.css'
 import clsx from 'src/utils/clsx'
+import { FieldProp } from 'src/types/login'
+import { loginAPI } from 'src/services/login'
+import { useNavigate } from 'react-router-dom'
 
 
 const Login: React.FC = () => {
+  const router = useNavigate();
+
+  const loginSubmit = async (values: FieldProp) => {
+    console.log('res values', values)
+    try {
+      const res = await loginAPI(values)
+      localStorage.setItem('login-status', 'true')
+      if (res) router('/home')
+    } catch (err) {
+      console.log('res in err', err)
+    }
+  }
   return (
-    <Formik initialValues={{ email: '', password: '' }} onSubmit={(value) => { console.log(value) }}>
+    <Formik initialValues={{ email: '', password: '' }} onSubmit={loginSubmit}>
       <Form className={clsx(styles.form, styles.form_flex)}>
         <label id='email'>Email</label>
         <Field name="email" type="email" placeholder="Email" />

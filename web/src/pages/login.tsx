9d85059ef1e9073a4daa3cd458456e7d9e05ac09
Subin_S22 <ssubin615@gmail.com>
2022-05-27 16:33:41 +0530

    login

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -10,10 +10,10 @@ const Login: React.FC = () => {
     <Formik initialValues={{ email: '', password: '' }} onSubmit={(value) => { console.log(value) }}>
       <Form className={clsx(styles.form, styles.form_flex)}>
         <label id='email'>Email</label>
-        <Field name="email" type="email" />
+        <Field name="email" type="email" placeholder="Email" />
         <ErrorMessage name="email" component="div" />
         <label id='password'>Password</label>
-        <Field name="password" type="password" />
+        <Field name="password" type="password" palceholder="Password" />
         <ErrorMessage name="password" component="div" />
         <Button buttonName='Submit' buttonClick={() => { '' }} type="submit" />
       </Form>
